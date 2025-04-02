176bda3bce62f0c1d0a74f65f93dfa34d7009516
Musyaffaa2 <musyaffaarwin@gmail.com>
2025-04-02 22:12:27 +0700

    update style button delete

--- components/custom-button.js
+++ components/custom-button.js
@@ -1,23 +1,22 @@
 class CustomButton extends HTMLElement {
-    constructor() {
-      super();
-      this.attachShadow({ mode: "open" });
-      this.shadowRoot.innerHTML = `
-        <style>
-            button {
-                padding: 10px;
-                font-size: 16px;
-                color: white;
-                background: ${this.getAttribute("color") || "gray"};
-                border: none;
-                border-radius: 5px;
-                cursor: pointer;
-            }
-        </style>
-        <button>${this.getAttribute("text")}</button>
-      `;
-    }
+  constructor() {
+    super();
+    this.attachShadow({ mode: "open" });
+    this.shadowRoot.innerHTML = `
+      <style>
+          button {
+              padding: 10px;
+              font-size: 16px;
+              color: white;
+              background: #007bff; /* Ubah warna menjadi biru */
+              border: none;
+              border-radius: 5px;
+              cursor: pointer;
+          }
+      </style>
+      <button>${this.getAttribute("text")}</button>
+    `;
   }
-  
-  customElements.define("custom-button", CustomButton);
-  
+}
+
+customElements.define("custom-button", CustomButton);
